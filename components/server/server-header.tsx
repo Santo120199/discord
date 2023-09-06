b2af52a5d594c4960763cfef8a7ae6f115e6f12e
davidesantopietro <davide.santopietro@itsvil.it>
2023-09-06 15:26:15 +0200

    Server Settings done

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -66,7 +66,7 @@ const ServerHeader: React.FC<ServerHeaderProps> = ({ role, server }) => {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer" onClick={() => onOpen("editServer",  { server })}>
             Server Settings
             <Settings className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>

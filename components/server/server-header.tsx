9daa3003d348bdb259e675f214d115673904225d
davidesantopietro <davide.santopietro@itsvil.it>
2023-09-07 17:05:19 +0200

    Create Channel done

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -78,7 +78,7 @@ const ServerHeader: React.FC<ServerHeaderProps> = ({ role, server }) => {
           </DropdownMenuItem>
         )}
         {isModerator && (
-          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer" onClick={() => onOpen("createChannel")}>
             Create Channel
             <PlusCircle className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
